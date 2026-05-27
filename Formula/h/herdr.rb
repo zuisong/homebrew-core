8646cf66ff058b8c8e3b7a5a3ab7643f0df1e769
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-27 20:57:52 +0000

    herdr 0.6.4

--- Formula/h/herdr.rb
+++ Formula/h/herdr.rb
@@ -1,8 +1,8 @@
 class Herdr < Formula
   desc "Agent multiplexer that lives in your terminal"
   homepage "https://herdr.dev"
-  url "https://github.com/ogulcancelik/herdr/archive/refs/tags/v0.6.2.tar.gz"
-  sha256 "3a0563db82a3c574a26c910a7c61617ca3d80864715b6f5572efe09d7e95ec8f"
+  url "https://github.com/ogulcancelik/herdr/archive/refs/tags/v0.6.4.tar.gz"
+  sha256 "4f83fb17d6ce0796bf50b19ee4919806d55cdb66cfc7f7b5b5893d4f8978bfa1"
   license "AGPL-3.0-or-later"
   head "https://github.com/ogulcancelik/herdr.git", branch: "master"
 
